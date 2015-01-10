fd2bf21a5931c866d93ed70e812c09b8d726ad88
yhou <yuchen.hou@wsu.edu>
2015-01-10 01:34:38 -0800

    cleanup

--- analyzer.py
+++ analyzer.py
@@ -12,8 +12,21 @@
 # import sys
 # sys.stdout = Unbuffered(sys.stdout)
 
-# time_stamp = strftime('%Y-%m-%d-%H-%M-%S')
-# check_call(['mv', TRACE, TRACE[:5] + '-' + time_stamp + TRACE[5:]])
+# data_file = 'web-Google_sorted'
+# with open(data_file + ".csv", 'r') as old_data:
+#     with open(data_file + ".final.csv", 'w') as new_data:
+#         data_reader = reader(old_data)
+#         data_writer = writer(new_data)
+#         for record in data_reader:
+#             thread_count = float(record[3])
+#             if thread_count == 1:
+#                 singlethread_runtime = float(record[4])
+#                 speedup = 1
+#             else:
+#                 multithread_runtime = float(record[4])
+#                 speedup = singlethread_runtime / multithread_runtime
+#             record.append(' ' + str(speedup))
+#             data_writer.writerow(record)
 
 from time import strftime
 from itertools import cycle
@@ -37,46 +50,27 @@ from matplotlib.pyplot import show
 from matplotlib.pyplot import savefig
 from matplotlib.pyplot import plotfile
 from matplotlib.font_manager import FontProperties
+
 TRACE = 'trace.dat'
 FIGURE = 'trace.png'
 DOCUMENT = 'architect'
-
-# data_file = 'web-Google_sorted'
-# with open(data_file + ".csv", 'r') as old_data:
-#     with open(data_file + ".final.csv", 'w') as new_data:
-#         data_reader = reader(old_data)
-#         data_writer = writer(new_data)
-#         for record in data_reader:
-#             thread_count = float(record[3])
-#             if thread_count == 1:
-#                 singlethread_runtime = float(record[4])
-#                 speedup = 1
-#             else:
-#                 multithread_runtime = float(record[4])
-#                 speedup = singlethread_runtime / multithread_runtime
-#             record.append(' ' + str(speedup))
-#             data_writer.writerow(record)
-
-data = genfromtxt(TRACE, delimiter = '\t', names = True)
-styles = cycle(('--', '-.', '-', ':'))
-font_properties = FontProperties()
-font_properties.set_size('small')
-figure()
-for column in data.dtype.names:
-    # plot(data[column], label = column, linestyle = styles.next())
-    if (column == 'score'):
-        subplot(311)
+time_stamp = strftime('-%Y-%m-%d')
+check_call(['mv', FIGURE,FIGURE.replace('.', time_stamp + '.')])
+print('what?')
+def analyze():
+    data = genfromtxt(TRACE, delimiter = '\t', names = True)
+    font_properties = FontProperties()
+    font_properties.set_size('small')
+    figure()
+    for column in data.dtype.names:
+        if (column == 'quality'):
+            subplot(311)
+        if (column == 'latency'):
+            subplot(312)
+        if (column == 'power'):
+            subplot(313)
         plot(data[column], label = column)
         legend(loc = 'upper right', prop = font_properties).get_frame().set_alpha(.1)
-    if (column == 'latency'):
-        subplot(312)
-        plot(data[column], label = column)
-        legend(loc = 'upper right', prop = font_properties).get_frame().set_alpha(.1)
-    if (column == 'path_length'):
-        subplot(313)
-        plot(data[column], label = column)
-        legend(loc = 'upper right', prop = font_properties).get_frame().set_alpha(.1)
-xlabel('step')
-# legend(loc = 'upper right', prop = font_properties).get_frame().set_alpha(.1)
-savefig(FIGURE, dpi = 200)
-check_call(['pdflatex', DOCUMENT])
+    xlabel('step')
+    # savefig(FIGURE, dpi = 200)
+    check_call(['pdflatex', DOCUMENT])
